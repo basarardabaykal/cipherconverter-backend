8a4290a10bb429bab1bd4cee9a22262da6aefecb
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 18:25:39 +0300

    fix indention

--- cipherconverter/ciphers/serializers/caeser.py
+++ cipherconverter/ciphers/serializers/caeser.py
@@ -5,12 +5,12 @@ from models import Caesar
 class CaesarSerializer(BaseCipherSerializer):
     class Meta:
         model = Caesar
-    fields = [
-        "input_text",
-        "key",
-        "operation",
-        "output_text",
-        "created_at",
-        "created_by",
-    ]
+        fields = [
+            "input_text",
+            "key",
+            "operation",
+            "output_text",
+            "created_at",
+            "created_by",
+        ]
     read_only_fields = ["output_text, created_at, created_by"]
