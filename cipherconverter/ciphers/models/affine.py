f67b3a9d8a2e529621d41967cb5e8443ea8d370d
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 13:04:58 +0300

    create Affine cipher model

---
+++ cipherconverter/ciphers/models/affine.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from models import CipherBase
+
+class Affine(CipherBase):
+    class A(models.IntegerChoices):
+        A1 = 1, "1"
+        A3 = 3, "3"
+        A5 = 5, "5"
+        A7 = 7, "7"
+        A9 = 9, "9"
+        A11 = 11, "11"
+        A15 = 15, "15"
+        A17 = 17, "17"
+        A19 = 19, "19"
+        A21 = 21, "21"
+        A23 = 23, "23"
+        A25 = 25, "25"
+            
+    a = models.IntegerField(choices=A.choices)
+    b = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(25)])
