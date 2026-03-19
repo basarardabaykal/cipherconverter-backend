c4327473281d85fd10dbd143c78dc6a1be954632
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 15:11:22 +0300

    implement caesar serializer

--- cipherconverter/ciphers/serializers/caeser.py
+++ cipherconverter/ciphers/serializers/caeser.py
@@ -6,5 +6,11 @@ class CaesarSerializer(BaseCipherSerializer):
     class Meta:
         model = Caesar
     fields = [
-        ""
+        "input_text",
+        "key",
+        "operation",
+        "output_text",
+        "created_at",
+        "created_by",
     ]
+    read_only_fields = ["output_text, created_at, created_by"]
