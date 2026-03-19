833ed527732aea84fd1fda89b5cf6e14beaf294e
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 12:44:39 +0300

    implement Caesar model

--- cipherconverter/ciphers/models/caesar.py
+++ cipherconverter/ciphers/models/caesar.py
@@ -1,5 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from models import CipherBase
 
 class Caesar(CipherBase):
-    pass
+    key = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(26)])
