5dc59883a8fdad66df4ba9303d71c3caf48e90bf
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 12:20:45 +0300

    change related_name to be dynamic

--- cipherconverter/ciphers/models/base.py
+++ cipherconverter/ciphers/models/base.py
@@ -7,7 +7,11 @@ class Operation(models.TextChoices):
 
 class CipherBase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="created_ciphers")
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="created_%(class)s",
+    )
     input_text = models.TextField()
     output_text = models.TextField()
     operation = models.CharField(max_length=10, choices=Operation.choices)
