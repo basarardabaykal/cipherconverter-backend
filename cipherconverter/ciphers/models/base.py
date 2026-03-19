4720b662e77ec7eb3623dacac22ea3964c8dd7b4
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 12:17:05 +0300

    create base model for ciphers

---
+++ cipherconverter/ciphers/models/base.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+class Operation(models.TextChoices):
+    ENCRYPT = "encrypt", "Encrypt"
+    DECRYPT = "decrypt", "Decrypt"
+
+class CipherBase(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="created_ciphers")
+    input_text = models.TextField()
+    output_text = models.TextField()
+    operation = models.CharField(max_length=10, choices=Operation.choices)
+
+    class Meta:
+        abstract = True
