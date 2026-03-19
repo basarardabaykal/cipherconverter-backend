927bd23d903322a6d472ba20f66764e8da597372
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 15:15:35 +0300

    create affine serializer

---
+++ cipherconverter/ciphers/serializers/affine.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from serializers import BaseCipherSerializer
+from models import Affine
+
+class AffineSerializer(BaseCipherSerializer):
+    class Meta:
+        model = Affine
+        fields = [
+            "input_text",
+            "a",
+            "b",
+            "operation",
+            "output_text",
+            "created_at",
+            "created_by"
+        ]
+        read_only_fields = ["output_text", "created_at", "created_by"]
