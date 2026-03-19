a31e27efdd47ea8c30ba048888673b0a0b021a05
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-19 13:05:55 +0300

    change location of operation values class

--- cipherconverter/ciphers/models/base.py
+++ cipherconverter/ciphers/models/base.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-class Operation(models.TextChoices):
-    ENCRYPT = "encrypt", "Encrypt"
-    DECRYPT = "decrypt", "Decrypt"
-
 class CipherBase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
@@ -14,6 +10,11 @@ class CipherBase(models.Model):
     )
     input_text = models.TextField()
     output_text = models.TextField()
+
+    class Operation(models.TextChoices):
+        ENCRYPT = "encrypt", "Encrypt"
+        DECRYPT = "decrypt", "Decrypt"
+
     operation = models.CharField(
         max_length=10, choices=Operation.choices, db_index=True
     )
