02cf3a361d0b31dddd5c1dcd5e96d4cd5923bbd2
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-01 22:09:55 +0300

    change models imports to better format

--- cipherconverter/ciphers/models/affine.py
+++ cipherconverter/ciphers/models/affine.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from models import BaseCipher
+from .base import BaseCipher
 
 class Affine(BaseCipher):
     class A(models.IntegerChoices):
--- cipherconverter/ciphers/models/caesar.py
+++ cipherconverter/ciphers/models/caesar.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from models import BaseCipher
+from .base import BaseCipher
 
 class Caesar(BaseCipher):
     key = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(26)])
