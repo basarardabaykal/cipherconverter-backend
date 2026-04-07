45094aece8b88d768125ebf33bcc2f84fbdb862f
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-07 11:44:13 +0300

    Revert "Enable WhiteNoise and set STATIC_ROOT"
    This reverts commit 19a6a8aaccc9d22c56d6095a89f21b9390c40fd0.

--- cipherconverter/config/settings.py
+++ cipherconverter/config/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -120,4 +119,3 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
