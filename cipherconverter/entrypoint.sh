fe0504c04ed55708735a89a7aaaf04ed9e024f64
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-07 11:41:35 +0300

    create entrypoint for backend container

---
+++ cipherconverter/entrypoint.sh
@@ -0,0 +1,13 @@
+echo "Waiting for postgres..."
+while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER
+do
+  echo "Database is not ready yet. Retrying in 2 seconds..."
+  sleep 2
+done
+echo "PostgreSQL started"
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
