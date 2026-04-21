a0d7f6fe72439488388c818a7f34f270741d3b12
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-21 13:18:42 +0300

    create healthcheck endpoint

--- cipherconverter/config/urls.py
+++ cipherconverter/config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import healthcheck
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/healthcheck/", healthcheck, name="healthcheck"),
     path("api/auth/", include("users.urls")),
     path("api/ciphers/", include("ciphers.urls")),
 ]
