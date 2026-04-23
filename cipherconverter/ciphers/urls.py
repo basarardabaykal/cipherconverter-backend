1c4b368b8c045d0468f1f90193f352d9ac652efa
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-23 14:24:35 +0300

    register otp url

--- cipherconverter/ciphers/urls.py
+++ cipherconverter/ciphers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CaesarViewSet, AffineViewSet, ColumnarViewSet
+from .views import CaesarViewSet, AffineViewSet, ColumnarViewSet, OTPViewSet
 
 urlpatterns = [
     path("caesar/", CaesarViewSet.as_view({"post": "create"}), name="ciphers-caesar"),
     path("affine/", AffineViewSet.as_view({"post": "create"}), name="ciphers-affine"),
     path("columnar/", ColumnarViewSet.as_view({"post": "create"}), name="ciphers-columnar"),
+    path("otp/", OTPViewSet.as_view({"post": "create"}), name="ciphers-otp"),
 ]
