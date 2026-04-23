f2d3d17692ee0794d63785b7f3bc3b2371ed7e9c
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-23 11:48:19 +0300

    add columnar model and serializer to __init__

--- cipherconverter/ciphers/models/__init__.py
+++ cipherconverter/ciphers/models/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseCipher
 from .caesar import Caesar
 from .affine import Affine
+from .columnar import Columnar
--- cipherconverter/ciphers/serializers/__init__.py
+++ cipherconverter/ciphers/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseCipherSerializer
 from .caesar import CaesarSerializer
 from .affine import AffineSerializer
+from .columnar import ColumnarSerializer
