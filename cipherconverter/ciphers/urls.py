11821548ce9dedf11706ff789fa65fec55e7aa18
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-23 12:10:00 +0300

    register columnar view

--- cipherconverter/ciphers/urls.py
+++ cipherconverter/ciphers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CaesarViewSet, AffineViewSet
+from .views import CaesarViewSet, AffineViewSet, ColumnarViewSet
 
 urlpatterns = [
     path("caesar/", CaesarViewSet.as_view({"post": "create"}), name="ciphers-caesar"),
     path("affine/", AffineViewSet.as_view({"post": "create"}), name="ciphers-affine"),
+    path("columnar/", ColumnarViewSet.as_view({"post": "create"}), name="ciphers-columnar"),
 ]
